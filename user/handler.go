578b0bb044036c3f00f0ebfca779cb568c1a7ce0
David Jacobs <david.jacobs@tangramflex.com>
2019-01-29 16:36:30 -0500

    Add samle user route annotation

---
+++ user/handler.go
@@ -0,0 +1,35 @@
+package user
+
+// swagger:operation GET /user/{id} user User
+//
+// Returns a user object
+// ---
+// consumes:
+// - application/json
+// produces:
+// - application/json
+// parameters:
+// - name: id
+//   in: path
+//   description: Id of the user
+//   required: true
+//   type: string
+// responses:
+//   '200':
+//     description: The user object
+//     type: string
+func GetUser() {
+
+}
+
+func postUser() {
+
+}
+
+func putUser() {
+
+}
+
+func deleteUser() {
+
+}
