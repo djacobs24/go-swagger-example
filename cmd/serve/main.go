4fc28884d15ec66cfac2a210c410818b4ab56d19
David Jacobs <david.jacobs@tangramflex.com>
2019-01-30 11:11:55 -0500

    Add swagger info and inport models

--- cmd/serve/main.go
+++ cmd/serve/main.go
@@ -1,6 +1,20 @@
-//     Schemes: http
-//     Host: localhost:8080
+// Go Swagger Example API
+//
+// The purpose of this application is to provide an application
+// that is using plain go code to define an API
+//
+// This should demonstrate all the possible comment annotations
+// that are available to turn go code into a fully compliant swagger 2.0 spec
+//
+// Terms Of Service:
+//
+// There are no TOS at this moment.
+//
+//     Schemes: http, https
+//     Host: localhost
+//     BasePath: /
 //     Version: 0.0.1
+//     Contact: David Jacobs<dav.david.jacobs@gmail.com>
 //
 //     Consumes:
 //     - application/json
@@ -13,9 +27,14 @@ package main
 
 import (
 	// Imported so the swagger generator will hit the user package
+	"fmt"
+
+	"github.com/djacobs24/go-swagger-example/models"
 	"github.com/djacobs24/go-swagger-example/user"
 )
 
 func main() {
 	user.GetUser()
+	user := models.User{}
+	fmt.Print(user)
 }
